d5fe948f7716ebb2905067a325049b39afed21be
Fernanda leite <dev.fernandaleite@gmail.com>
2021-06-25 19:37:59 -0300

    feat: create page new room

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,41 @@
+import { Link } from 'react-router-dom'
+
+import illustrationImg from '../assets/images/illustration.svg'
+import logoImg from '../assets/images/logo.svg'
+
+import '../styles/auth.scss'
+
+import { Button } from '../components/Button'
+
+export function NewRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e repostas" />
+        <h1>Crie salas de Q&amp;A ao-vivo</h1>
+        <p>Tire as dúvidas da sua audiência em tempo-real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Let me ask" />
+          
+          <h2>Crie uma nova sala</h2>
+
+          <form>
+            <input 
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+
+          <p>
+            Quer entrar em uma sala já existente? <Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  )
+}
