e93b18a85252708463c4a64cdebecb51b5a12f56
Fernanda leite <dev.fernandaleite@gmail.com>
2021-07-07 23:50:57 -0300

    feat: add modal confirmation

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,14 +1,18 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import Modal from 'react-modal';
 
 import { useRoom } from '../hooks/useRoom'
 
 import logoImg from '../assets/images/logo.svg'
+import closeImg from '../assets/images/close.svg'
 
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
 import { Question } from '../components/Question'
 
 import '../styles/room.scss'
+import '../styles/room-modal.scss'
 
 interface RoomParams {
   id: string;
@@ -17,6 +21,15 @@ interface RoomParams {
 export function AdminRoom() {
   const { id } = useParams<RoomParams>()
   const { questions, title } = useRoom(id);
+  const [isOpen, setIsOpen] = useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   return (
     <div id="page-room">
@@ -26,7 +39,7 @@ export function AdminRoom() {
 
           <div>
             <RoomCode code={id} />
-            <Button isOutlined>Encerrar</Button>
+            <Button isOutlined onClick={openModal} type="button">Encerrar</Button>
           </div>
         </div>
       </header>
@@ -50,6 +63,30 @@ export function AdminRoom() {
           )}
         </div>
       </main>
+
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={closeModal}
+        className="room-modal"
+        overlayClassName="room-modal-overlay"
+        contentLabel="Modal de confirmação para encerramento de sala"
+      >
+        <img src={closeImg} alt="Encerrar sala" />
+
+        <h2>Encerrar sala</h2>
+
+        <p>Tem certeza que você deseja encerrar esta sala?</p>
+
+        <div className="actions">
+          <button className="cancel-button" type="button" onClick={closeModal}>
+            Cancelar
+          </button>
+
+          <button className="accept-button" type="button">
+            Sim, encerrar
+          </button>
+        </div>
+      </Modal>
     </div>
   )
 }
