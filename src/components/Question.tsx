c4d8c1fd9052b0f4223c5ddb6948005951163878
Fernanda leite <dev.fernandaleite@gmail.com>
2021-07-09 19:51:17 -0300

    feat: add answered and highlighted questions

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react'
+import cx from 'classnames'
 
 import { UserInfo } from './UserInfo'
 
@@ -10,12 +11,20 @@ interface QuestionProps {
     name: string;
     avatar: string;
   },
-  children: ReactNode
+  children: ReactNode;
+  isAnswered?: boolean;
+  isHighlighted?: boolean;
 }
 
-export function Question({ content, author, children }: QuestionProps) {
+export function Question({ content, author, children, isAnswered = false, isHighlighted = false }: QuestionProps) {
   return (
-    <div className="question">
+    <div 
+      className={cx(
+        'question',
+        { answered: isAnswered },
+        { highlighted: isHighlighted && !isAnswered }
+      )}
+    >
       <p>{content}</p>
       <footer>
         <UserInfo avatar={author.avatar} name={author.name} />
--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -7,8 +7,10 @@ import { database } from '../services/firebase';
 
 import logoImg from '../assets/images/logo.svg'
 import closeImg from '../assets/images/close.svg'
-import deleteImg from '../assets/images/delete.svg'
 import excludeImg from '../assets/images/exclude.svg'
+import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 
 import { Button } from '../components/Button'
 import { RoomCode } from '../components/RoomCode'
@@ -63,6 +65,18 @@ export function AdminRoom() {
     setModalQuestionIsOpen(false)
   }
 
+  async function handleCheckQuestionAsAnswered(questionId: string) {
+    await database.ref(`rooms/${id}/questions/${questionId}`).update({
+      isAnswered: true,
+    })
+  }
+
+  async function handleHighlightQuestion(questionId: string) {
+    await database.ref(`rooms/${id}/questions/${questionId}`).update({
+      isHighlighted: true,
+    })
+  }
+
   return (
     <div id="page-room">
       <header>
@@ -86,7 +100,25 @@ export function AdminRoom() {
         <div className="question-list">
           {questions.length > 0 ? (
             questions.map((question) => (
-              <Question key={question.id} author={question.author} content={question.content}>
+              <Question key={question.id} author={question.author} content={question.content} isAnswered={question.isAnswered} isHighlighted={question.isHighlighted}>
+                {!question.isAnswered && (
+                  <>
+                    <button 
+                      type="button"
+                      onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                    >
+                      <img src={checkImg} alt="Marcar pergunta como respondida" />
+                    </button>
+
+                    <button
+                      type="button"
+                      onClick={() => handleHighlightQuestion(question.id)}
+                    >
+                      <img src={answerImg} alt="Destacar pergunta" />
+                    </button>
+                  </>
+                )}
+
                 <button 
                   type="button"
                   onClick={() => openModalQuestion(question.id)}
